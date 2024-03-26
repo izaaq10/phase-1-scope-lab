1680b6411dded5227b0ef77d1282409acdab9b8a
Izaaq <odongoizaaq@gmail.com>
2024-03-26 05:17:22 -0400

    Adds the scope-lab

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+var customerName = "bob";
+
+function upperCaseCustomerName() {
+    if(customerName){
+        customerName = customerName.toUpperCase()    }
+   
+    }
+
+    function setBestCustomer(){
+        bestCustomer = "not bob"
+    }
+   
+   
+    function overwriteBestCustomer(){
+        bestCustomer = "maybe bob"
+    }
+
+    const leastFavoriteCustomer = "Peter"
+    function changeLeastFavoriteCustomer(){
+        leastFavoriteCustomer = "Bob"
+    }
